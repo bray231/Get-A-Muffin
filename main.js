d02d8483c1db07b67365eb80abbb8de4ee02fa43
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-03-07 14:55:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var num_muffins = 0;
     
 function getMuffin(){
+  num_muffins = convertToDecimal(num_muffins);
   num_muffins = num_muffins + 1;
   var elem = document.createElement("img");
   elem.src = 'muffin.jpg';
@@ -8,5 +9,13 @@ function getMuffin(){
   elem.setAttribute("width", "24");
   elem.setAttribute("alt", "Muffin!");
   document.getElementById("placehere").appendChild(elem);
-  document.getElementById("muffin_counter").innerHTML = "Muffins: " + num_muffins;
+  document.getElementById("muffin_counter").innerHTML = "Muffins: " + convertToBinary(num_muffins);
+}
+
+function convertToBinary(num){
+  return parseInt(num, 10).toString(2);
+}
+
+function convertToDecimal(num){
+    return parseInt(num, 2).toInteger(10);
 }
