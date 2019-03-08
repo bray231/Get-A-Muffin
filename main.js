a105dd7dd5c2d4d0b2bd0a7275071d7b8825db7e
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-03-08 14:55:06 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var num_muffins = 0;
+    
+function getMuffin(){
+  num_muffins = num_muffins + 1;
+  var elem = document.createElement("img");
+  var rnd = Math.floor(Math.random() * 3);
+  
+  if(rnd == 0){
+    rnd = 1;
+  }
+  
+  if(rnd == 1){
+    elem.src = 'chocolate_chip.jpg';
+  }
+  
+  if(rnd == 2){
+    elem.src = 'chocolate.jpg';
+  }
+  
+  if(rnd == 3){
+    elem.src = 'plain.jpg';
+  }
+  
+  elem.setAttribute("height", "24");
+  elem.setAttribute("width", "24");
+  elem.setAttribute("alt", "Muffin!");
+  document.getElementById("placehere").appendChild(elem);
+  document.getElementById("muffin_counter").innerHTML = "Muffins: " + num_muffins;
+}
