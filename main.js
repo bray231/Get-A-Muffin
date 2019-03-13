39600c41b10014549cdfccf296fa272b8e1cdaa6
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-03-12 18:01:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-var isBase10()
+var isBase10 = True;
+var isBinary = False;
 var num_muffins = 0;
 
 function getMuffin(){
+  if(isBase10 != True){
+    num_muffins = convertToBase10(num_muffins);
+  }
+  
   num_muffins = num_muffins + 1;
+  
+  if(isBinary == True){
+     num_muffins = convertToBinary(num_muffins);
+  }
+  
   var elem = document.createElement("img");
   var rnd = Math.floor(Math.random() * 6);
   if(rnd == 0){
@@ -28,6 +38,14 @@ function getMuffin(){
   elem.setAttribute("alt", "Muffin!");
   document.getElementById("placehere").appendChild(elem);
   document.getElementById("muffin_counter").innerHTML = "Muffins: " + num_muffins;
+}
+
+function convertToBinary(num){
+  return parseInt(num, 10).toString(2);
+}
+
+function convertToBase10(num){
+    return parseInt(num, 2).toString(10);
 }
 
 function setBase10(){
