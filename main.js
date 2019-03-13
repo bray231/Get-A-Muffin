64cce0fc4e33ecb914e704f8ca81600ae4cfe706
Mommygohi <46762402+Mommygohi@users.noreply.github.com>
2019-03-13 15:17:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 var num_muffins = 0;
+var num_muffins_binary = 0;
+var num_muffins_hexa = 0;
+var num_muffins_octal = 0;
+var num_muffins_base36 = 0;
+var counter = 0;
 
 function getMuffin(){
-  num_muffins = num_muffins + 1;
+  counter += 1;
+  num_muffins = counter;
+  num_muffins_binary = convertToBinary(counter);
+  num_muffins_hexa = convertToHexa(counter);
+  num_muffins_octal = convertToOctal(counter);
+  num_muffins_base36 = convertToBase36(counter);
   var elem = document.createElement("img");
   var rnd = Math.floor(Math.random() * 6);
   if(rnd == 0){
@@ -27,4 +37,24 @@ function getMuffin(){
   elem.setAttribute("alt", "Muffin!");
   document.getElementById("placehere").appendChild(elem);
   document.getElementById("muffin_counter").innerHTML = "Muffins: " + num_muffins;
+  document.getElementById("muffin_counter_binary").innerHTML = num_muffins_binary;
+  document.getElementById("muffin_counter_hexa").innerHTML = num_muffins_hexa;
+  document.getElementById("muffin_counter_octal").innerHTML = num_muffins_octal;
+  document.getElementById("muffin_counter_base36").innerHTML = num_muffins_base36;
+}
+
+function convertToBinary(num){
+  return parseInt(num, 10).toString(2);
+}
+
+function convertToHexa(num){
+  return parseInt(num, 10).toString(6);
+}
+
+function convertToOctal(num){
+  return parseInt(num, 10).toString(8);
+}
+
+function convertToBase36(num){
+  return parseInt(num, 10).toString(36);
 }
